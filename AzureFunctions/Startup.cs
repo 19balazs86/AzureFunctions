01f0f5173f4cc041f4e0303ff9d3977b282dc1c5
Balázs Keresztúri <19balazs86@gmail.com>
2019-06-26 20:37:29 +0100

    Add initial startup.

---
+++ AzureFunctions/Startup.cs
@@ -0,0 +1,21 @@
+﻿using AzureFunctions;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: WebJobsStartup(typeof(Startup))]
+namespace AzureFunctions
+{
+  public class Startup : IWebJobsStartup
+  {
+    public void Configure(IWebJobsBuilder builder)
+    {
+      configureServices(builder.Services);
+    }
+
+    private static void configureServices(IServiceCollection services)
+    {
+      
+    }
+  }
+}
